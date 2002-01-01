81cb0c13a7a3de5e63155b6a1b5f71b52f13e47f
restimel <restimel@gmail.com>
2002-01-01 01:18:23 +0100

    add i18n translation tool

---
+++ libs/translate-i18n.js
@@ -0,0 +1,41 @@
+var $$ = (function() {
+	var languages = ['en', 'fr'];
+	var translation = {
+
+	};
+
+	var lng = 'en';
+
+	/**
+	 * replace token key by matching translation
+	 * strings are replace by words depending of rplcW
+	 */
+	function mathing(key, rplcW) {
+		var w = translation[key];
+
+		if (typeof w === 'undefined') {
+			console.warn('Translation missing', key);
+			return key;
+		}
+
+		w = w[lng];
+
+		if (rplcW) {
+			w = w.replace(/%s/g, rplcW['%s']);
+		}
+
+		return w;
+	};
+
+	matching.changeLng = function(lg) {
+		var l = languages.indexOf(lg);
+
+		if (l === -1) {
+			console.warn('language unknown', lg);
+		} else {
+			lng = l;
+		}
+	};
+
+	return  matching;
+})();
