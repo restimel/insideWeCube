0078fe73dc01a5f9a9fb2662d6511331254cf70e
Restimel <restimel@gmail.com>
2014-06-09 23:39:24 +0200

    upgrade translation

--- libs/translate-i18n.js
+++ libs/translate-i18n.js
@@ -1,8 +1,6 @@
 var $$ = (function() {
 	var languages = ['en', 'fr'];
-	var translation = {
-
-	};
+	var translation = {};
 
 	var lng = 'en';
 
@@ -10,21 +8,102 @@ var $$ = (function() {
 	 * replace token key by matching translation
 	 * strings are replace by words depending of rplcW
 	 */
-	function matching(key, rplcW) {
-		var w = translation[key];
+	function matching(key) {
+		var wo = translation[key],
+			w,
+			keys = Array.prototype.slice.call(arguments, 1).reverse();
 
-		if (typeof w === 'undefined') {
+		if (typeof wo === 'undefined') {
 			console.warn('Translation missing', key);
-			return key;
+			w = key;
+		} else {
+
+			w = wo[lng];
+
+			if (typeof w === 'undefined') {
+				console.warn('Translation in "' + lng + '" is missing',  key);
+				w = wo['en'];
+
+				if (typeof w === 'undefined') {
+					w = key;
+				}
+			}
 		}
 
-		w = w[lng];
+		if (keys.length) {
+			if (keys.length === 1 && keys[0] instanceof Array) {
+				keys = keys[0];
+			}
 
-		if (rplcW) {
-			w = w.replace(/%s/g, rplcW['%s']);
+			w = w.replace(/%[^\s,;.!?:'"]/g, replaceKeys);
 		}
 
 		return w;
+
+		function replaceKeys(flag) {
+			if (flag === '%%') {
+				return '%';
+			}
+
+			var k = keys.pop();
+			if (typeof k === 'undefined') {
+				console.warn('Flag replacement missing for "' + w + '".\n' + m + 'does not have any value');
+				k = '';
+			}
+
+			switch(flag) {
+				case '%r':
+				case '%a':
+				case '%s':
+					return k.toString();
+				case '%c':
+					return k.toString()[0] || '';
+				case '%f':
+				case '%F':
+				case '%g':
+				case '%u':
+				case '%d':
+					return Number(k);
+				case '%G':
+					return Number(k).toString().toUpperCase();
+				case '%i':
+				case '%1':
+					return parseInt(k, 10);
+				case '%o':
+					return Number(k).toString(8);
+				case '%x':
+					return Number(k).toString(16);
+				case '%X':
+					return Number(k).toString(16).toUpperCase();
+				case '%e':
+					return Number(k).toExponential();
+				case '%E':
+					return Number(k).toExponential().toUpperCase();
+				case '%0':
+				case '%2':
+					return pad(k, 2);
+				case '%3':
+					return pad(k, 3);
+				case '%4':
+					return pad(k, 4);
+				case '%5':
+					return pad(k, 5);
+				case '%6':
+					return pad(k, 6);
+				case '%7':
+					return pad(k, 7);
+				case '%8':
+					return pad(k, 8);
+				case '%9':
+					return pad(k, 9);
+				case '%+':
+					return (Number(k) > 0 ? '+' : '') + Number(k);
+				default:
+					console.warn('unknown Flag (' + flag + ')');
+					keys.push(k);
+					return flag;
+			}
+		}
 	};
 
 	matching.changeLng = function(lg) {
@@ -36,6 +115,47 @@ var $$ = (function() {
 			lng = l;
 		}
 	};
+	
+	function pad(nb, digit) {
+		var i = parseInt(nb, 10).toString(),
+			str = nb.toString(),
+			n = Number(nb);
+
+		if (str.length === digit) {
+			return str;
+		}
+
+		if (str.length < digit) {
+			return pad('0' + str, digit);
+		}
+
+		if (i.length >= digit) {
+			return i;
+		}
+
+		return n.toFixed(digit - i.length);
+	}
+
+	function loadTranslation(){
+		var xhr = new XMLHttpRequest(),
+			user = '',
+			password = '';
+		
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState == 4 && (xhr.status == 200 || xhr.status == 0)) {
+				try {
+					translation = JSON.parse(xhr.responseText);
+				} catch(e) {
+					console.error('translation not loaded', e);
+				}
+			}
+		};
+		
+		xhr.open("GET", "../libs/translations.json", true, user, password);
+		xhr.send();
+	}
+
+	loadTranslation();
 
 	return  matching;
 })();
