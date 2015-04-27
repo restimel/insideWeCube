af3e448700a545665b8efc9af98d60d873517c44
Restimel <restimel@gmail.com>
2015-04-27 22:32:02 +0200

    update version

--- pages/main.js
+++ pages/main.js
@@ -35,7 +35,7 @@ function main(container){
 	Helper.mainLoaded();
 }
 
-main.version = '1.1.2';
+main.version = '1.1.3';
 
 main.message = (function() {
 	var container = document.createDocumentFragment(),
